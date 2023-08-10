a629764c65809ac5884c4f3ab6008c701253fdc9
Simphiwe06 <simphiwembuli06@gmail.com>
2023-08-10 03:28:55 +0200

    fixed errors previosly made to get intended output

--- Challenge 1/scripts.js
+++ Challenge 1/scripts.js
@@ -1,10 +1,10 @@
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
 
 // Only change below this line
 
-const primaryValid = /^\d+$/.test(primaryPhone);
-const secondaryValid = /^\d+$/.test(secondaryPhone);
+const primaryValid = !isNaN(primaryPhone);
+const secondaryValid = !isNaN(secondaryPhone);
 
 console.log('Primary phone is valid numerical string:', primaryValid);
 console.log('Secondary phone is valid numerical string:', secondaryValid);
